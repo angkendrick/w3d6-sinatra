b115e1030e81f8bafdd21348de9524f2d9a3abc9
Ken Ang <angkendrick@gmail.com>
2015-07-18 23:01:36 +0000

    added timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718221106) do
+ActiveRecord::Schema.define(version: 20150718225207) do
 
   create_table "messages", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.string "content"
-    t.string "url"
+    t.string   "title"
+    t.string   "author"
+    t.string   "content"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
