0e06fc9b1efaa61428e30c15cdec059972048113
Ken Ang <angkendrick@gmail.com>
2015-07-18 21:52:30 +0000

    completed until 11

--- app/actions.rb
+++ app/actions.rb
@@ -5,7 +5,30 @@ end
 
 get '/messages' do
   #'Message List here' <- displays as a string 
-  @messages = Message.all
+  @messages = Message.all # retrieves all messages from the db makes it available to erb in this block
   erb :'messages/index'
 end
+
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+post '/messages' do #handles http posts comming from form
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+  )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
 
