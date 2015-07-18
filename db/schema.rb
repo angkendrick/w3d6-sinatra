d47ce27bf92dec97d48696c7df099df529c5f570
Ken Ang <angkendrick@gmail.com>
2015-07-18 22:49:48 +0000

    added url links

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718201731) do
+ActiveRecord::Schema.define(version: 20150718221106) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "content"
+    t.string "url"
   end
 
 end
